c68811e0b2d9d54cc388dd49ab6573c291e92903
mahmoud mohammed saad <70963102+mahm0udsaad@users.noreply.github.com>
2023-08-23 11:53:02 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const {MongoClient} = require('mongodb')
-const client = new MongoClient(procces.env.DB_URL)
+const client = new MongoClient(process.env.DB_URL)
 const dotenv = require('dotenv').config()
 
 
